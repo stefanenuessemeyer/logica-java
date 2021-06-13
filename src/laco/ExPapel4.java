7f48f9e02a2da4b55d8f1bab191fde4d1df9759f
Stefane <stefane.03ng@gmail.com>
2021-06-13 17:08:51 -0300

    exercicio papel 4

---
+++ src/laco/ExPapel4.java
@@ -0,0 +1,15 @@
+package laco;
+
+public class ExPapel4 {
+    public static void main(String[] args) {
+
+        int x = 5;
+        int y = 0;
+
+        while ( x > 2 ){
+            System.out.print(x);
+            y = y + x;
+            x = x - 1;
+        }
+    }
+}
