7163b425881de6c9bd11e123388171a7210d2891
Your Name <mayconsouzamendes@gmail.com>
2021-06-16 19:30:14 -0300

    exercicio papel 5

--- src/estruturarepeticao/para/ExPapel5.java
+++ src/estruturarepeticao/para/ExPapel5.java
@@ -1,2 +1,15 @@
-package estruturarepeticao.para;public class ExPapel5 {
+package estruturarepeticao.para;
+
+public class ExPapel5 {
+    public static void main(String[] args) {
+
+        int x = 8;
+        int y = 3;
+
+        for (int i = 0; y < x; i++){
+            x = x - 2;
+            y = y + 1;
+            System.out.println(i);
+        }
+    }
 }
