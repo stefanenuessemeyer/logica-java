fda1b684eef4927e8048b60fcfb71856fe0c1589
Your Name <mayconsouzamendes@gmail.com>
2021-06-16 19:40:55 -0300

    exercicio papel 6

---
+++ src/estruturarepeticao/para/ExPapel6.java
@@ -0,0 +1,15 @@
+package estruturarepeticao.para;
+
+public class ExPapel6 {
+    public static void main(String[] args) {
+
+         int x = 4;
+         int y = 0;
+
+        for (int i = 0; i < x; i++){
+            y = y + i;
+         }
+
+        System.out.println(y);
+    }
+}
